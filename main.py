634296286183cc09f031cae291e08365b78fbec0
playmiar-dev <playmiar@gmail.com>
2025-12-25 14:23:25 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,49 +1,8 @@
-from kivy.app import App
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.textinput import TextInput
-from kivy.uix.button import Button
+from kivy.uix.label import Label
 
-
-class Calculator(GridLayout):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.cols = 4
-
-        self.display = TextInput(
-            font_size=32,
-            readonly=True,
-            halign="right",
-            multiline=False
-        )
-        self.add_widget(self.display)
-
-        buttons = [
-            "7", "8", "9", "/",
-            "4", "5", "6", "*",
-            "1", "2", "3", "-",
-            "0", ".", "=", "+"
-        ]
-
-        for btn in buttons:
-            button = Button(text=btn, font_size=24)
-            button.bind(on_press=self.on_button_press)
-            self.add_widget(button)
-
-    def on_button_press(self, instance):
-        text = instance.text
-
-        if text == "=":
-            try:
-                self.display.text = str(eval(self.display.text))
-            except:
-                self.display.text = "Error"
-        else:
-            self.display.text += text
-
-
-class CalculatorApp(App):
+class MyApp(App):
     def build(self):
-        return Calculator()
+        return Label(text="Hello GitHub Actions!")
 
-
-CalculatorApp().run()
+if _name_ == "_main_":
+    MyApp().run()
